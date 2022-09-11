ee654995891aa978d654f82a44e28710518f45d1
Omkar Dhomane <omkardhomane@gmail.com>
2022-09-11 09:30:37 -0400

    Cloudmaster First Commit

---
+++ cloudmaster/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cloudmaster.users.models import User
+from cloudmaster.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
